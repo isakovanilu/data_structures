9da71b4bc28d431f6af660070b64eb26ea780718
Nilu Isakova <niluisakova@gmail.com>
2023-07-30 23:10:19 -0500

    added class Node

--- ds_py/linked_lists.py
+++ ds_py/linked_lists.py
@@ -1,1 +1,13 @@
 # information about LinkedList in python and its implementation
+# a linkedlis is a ds that conatins a sequence of elements, where each element pointing to the next element in the sequence
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+    self.head = None
+
+
+# TODO add append, insert, delete
