311d241b707bf6d10e3bc1b0cc048cfe505c862d
Nilu Isakova <niluisakova@gmail.com>
2023-08-10 22:47:55 -0500

    added winner checks

--- ds_py/test.py
+++ ds_py/test.py
@@ -6,14 +6,26 @@ def update_matrix3x3(matrix3x3, row, column, value):
     col_len = len(matrix3x3[0]) # column lenght
     if (row >= row_len or column >= col_len) or matrix3x3[row][column] != ' ': # if the coordinates greater than matrix size return the function
         print('Please enter different coordinates')
-        return
     else:
         for i in range(row_len): # loop throught rows
             for j in range(col_len): # loop throught columns
                 if i == row and j == column: # if row and columns values match for the given coordinates
                     matrix3x3[i][j] = value # assign the new values for those coordinates
 
+    for row in matrix3x3:
+        print(" | ".join(row))
+        print("-" * 9)
     return matrix3x3
 
-
 ## check the winner
+def winner_check(matrix3x3, value):
+    for x in range(3):
+        if (matrix3x3[x][0] == matrix3x3[x][1] == matrix3x3[x][2] == value) or (matrix3x3[0][x] == matrix3x3[1][x] == matrix3x3[2][x] == value):
+            return True
+    for y in range(3):
+        if (matrix3x3[y][0] == matrix3x3[y][1] == matrix3x3[y][2] == value) or (matrix3x3[0][y] == matrix3x3[1][y] == matrix3x3[2][y] == value):
+            return True
+    for x in range(0, 3, 2):
+        if matrix3x3[x][0] == matrix3x3[1][1] == matrix3x3[2-x][2]:
+            return True
+    return False
