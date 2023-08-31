0fbc3dd65d15c7890d3afa4a85278e00e2f7fcda
Nilu Isakova <niluisakova@gmail.com>
2023-08-31 10:29:13 -0500

    added insert method

--- ds_py/doubly_linked_list.py
+++ ds_py/doubly_linked_list.py
@@ -23,4 +23,30 @@ class DoublyLinkedList:
         self.head = node
         self.tail = node
         return "The DLL is successfully created"
- 
+    
+    def insertNode(self, nodeValue, location):
+        if self.head is None:
+            print("The node cannot be inserted into the list")
+        else:
+            newNode = Node(nodeValue)
+            if location == 0:
+                newNode.prev = None
+                newNode.next = self.head
+                self.head.prev = newNode
+                self.head = newNode
+            elif location == -1:
+                newNode.next = None
+                newNode.prev = self.tail
+                self.tail.next = newNode
+                self.tail = newNode
+            else:
+                tempNode = self.head
+                index = 0
+                while index < location - 1:
+                    tempNode = tempNode.next
+                    index += 1
+                    
+                newNode.next = tempNode.next
+                newNode.prev = tempNode
+                newNode.next.prev = newNode
+                tempNode.next = newNode
