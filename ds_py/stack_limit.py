655e257137205f12b59e376790924e70e9321f3e
Nilu Isakova <niluisakova@gmail.com>
2023-09-10 20:26:45 -0500

    added peek method

--- ds_py/stack_limit.py
+++ ds_py/stack_limit.py
@@ -33,3 +33,22 @@ class Stack:
         else:
             return self.list.pop()
     
+    def peek(self):
+        if self.isEmpty():
+            return "There is no element in the stack"
+        else:
+            return self.list[len(self.list)-1]
+        
+    def delete(self):
+        self.list = None
+        
+customStack = Stack(4)
+print(customStack.isEmpty())
+print(customStack.isFull())
+customStack.push(1)
+customStack.push(2)
+customStack.push(3)
+print(customStack)
+# customStack.pop()
+customStack.peek()
+print("after pop", customStack)
