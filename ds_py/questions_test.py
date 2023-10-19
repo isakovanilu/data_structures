e297490ad7c4be1f944a8aaace501f9296aa54bb
Nilu Isakova <niluisakova@gmail.com>
2023-10-19 12:12:38 -0500

    added function to convert decimal to binary

--- ds_py/questions_test.py
+++ ds_py/questions_test.py
@@ -152,4 +152,14 @@ def gcd(a, b):
     else:
         return gcd(b, a%b) 
     
-print(gcd(-48, 18))          
+print(gcd(-48, 18))
+
+def decimal_to_binary(n):
+    assert int(n) == n, 'The parameter should be integer'
+    if n == 0:
+        return 0
+    else:
+        return n%2 + 10 * decimal_to_binary(int(n/2))
+
+print(decimal_to_binary(12))
+
