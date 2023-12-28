aa46feb6e8d374204c4f0aea62a8bbf543ec34e9
Nilu Isakova <niluisakova@gmail.com>
2023-12-28 16:10:05 -0600

    Update linked_list_questions.py

--- ds_py/linked_list_questions.py
+++ ds_py/linked_list_questions.py
@@ -22,12 +22,17 @@ def remove_duplicates(ll):
     return ll.head
 
 
-def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-    new_list = None
-    current = head
-    while current:
-        next_node = current.next
-        current.next = new_list
-        new_list = current
-        current = next_node
-    return new_list
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        new_list = None
+        current = head
+        while current:
+            next_node = current.next
+            current.next = new_list
+            new_list = current
+            current = next_node
+        return new_list
