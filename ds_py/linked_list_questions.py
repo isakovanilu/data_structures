70839b23eedd8944eede322738c8d9cc266a9b8a
Nilu Isakova <niluisakova@gmail.com>
2023-12-28 16:08:46 -0600

    Update linked_list_questions.py

--- ds_py/linked_list_questions.py
+++ ds_py/linked_list_questions.py
@@ -20,3 +20,14 @@ def remove_duplicates(ll):
  
     ll.tail = prev_node
     return ll.head
+
+
+def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    new_list = None
+    current = head
+    while current:
+        next_node = current.next
+        current.next = new_list
+        new_list = current
+        current = next_node
+    return new_list
