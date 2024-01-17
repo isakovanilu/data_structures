79618a00783b7ab4ae2e220f711df1471ed6a3ce
Nilu Isakova <niluisakova@gmail.com>
2024-01-16 21:53:30 -0600

    added basic calculator function

--- ds_py/programming_tasks.py
+++ ds_py/programming_tasks.py
@@ -2,15 +2,15 @@ def basic_calculator():
     while True:
         a = int(input('Enter a number1: '))
         b = int(input('Enter a number2: '))
-        operation = input('Enter an arithmetic operations: like add, sub, mult, div ').lower().strip()
+        operation = input('Enter an arithmetic operations: like add/a, sub/s, mult/m, div/d  ').lower().strip()
 
-        if operation == 'add':
+        if operation in ['add', 'a']:
             result = a+b
-        elif operation == 'sub':
+        elif operation in ['sub', 's']:
             result = a-b
-        elif operation == 'mult':
+        elif operation in ['mult', 'm']:
             result = a*b
-        elif operation == 'div':
+        elif operation in ['div', 'd']:
             result = a/b if b != 0 else 'denominator cannot be zero'
         else:
             print('invalid input')
