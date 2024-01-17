a627e4bc2c07a066281598ab43a82cd25b91b9aa
Nilu Isakova <niluisakova@gmail.com>
2024-01-17 01:27:29 -0600

    number_guessing function

--- ds_py/programming_tasks.py
+++ ds_py/programming_tasks.py
@@ -1,42 +1,46 @@
 import random
 
+
 def basic_calculator():
-    """Basic Calculator: Create a program that takes 
-    two numbers as inputs and performs basic arithmetic 
-    operations (addition, subtraction, multiplication, division). 
-    This introduces the concept of variables, user input, 
+    """Basic Calculator: Create a program that takes
+    two numbers as inputs and performs basic arithmetic
+    operations (addition, subtraction, multiplication, division).
+    This introduces the concept of variables, user input,
     and basic arithmetic operations."""
     while True:
         a = int(input('Enter a number1: '))
         b = int(input('Enter a number2: '))
-        operation = input('Enter an arithmetic operations: like add/a, sub/s, mult/m, div/d  ').lower().strip()
+        operation = input(
+            'Enter an arithmetic operations: like add/a, sub/s, mult/m, div/d  ').lower().strip()
 
         if operation in ['add', 'a']:
-            result = a+b
+            result = a + b
         elif operation in ['sub', 's']:
-            result = a-b
+            result = a - b
         elif operation in ['mult', 'm']:
-            result = a*b
+            result = a * b
         elif operation in ['div', 'd']:
-            result = a/b if b != 0 else 'denominator cannot be zero'
+            result = a / b if b != 0 else 'denominator cannot be zero'
         else:
             print('invalid input')
         print('result: ', result)
-        continue_calculation = input("do you want to continue? (yes/y/no/n): ").lower().strip()
+        continue_calculation = input(
+            "do you want to continue? (yes/y/no/n): ").lower().strip()
         if continue_calculation not in ['yes', 'y']:
             break
 
 # basic_calculator()
 
+
 def number_guessing():
     """
-    Number Guessing Game: Develop a simple game where 
-    the program randomly selects a number, and the user
-    has to guess it. Provide hints like "too high" or "too low." 
-    This will teach control structures 
+    Number Guessing Game: Develop a simple game where
+    the program randomly selects a number, and the user has to guess it.
+    Provide hints like "too high" or "too low."
+    This will teach control structures
     (if-else statements) and the use of random number generation.
     """
-    rand_number = random.randint(1, 3)
+    rand_number = random.randint(1, 10)
     while True:
         guessed_number = int(input('Enter a number to guess: '))
         if rand_number < guessed_number:
@@ -46,5 +50,6 @@ def number_guessing():
         elif rand_number == guessed_number:
             print('You have guessed the number')
             break
-    
-number_guessing()    
+
+
+number_guessing()
