778ce785a4d581eb9f9913afb623852744e1a5d9
Nilu Isakova <niluisakova@gmail.com>
2024-01-16 21:46:39 -0600

    added basic calculator function

--- ds_py/programming_tasks.py
+++ ds_py/programming_tasks.py
@@ -1,18 +1,22 @@
 def basic_calculator():
-    a = int(input('Enter a number1: '))
-    b = int(input('Enter a number2: '))
-    operation = input('Enter an arithmetic operations: like add, sub, mult, div ').lower().strip()
+    while True:
+        a = int(input('Enter a number1: '))
+        b = int(input('Enter a number2: '))
+        operation = input('Enter an arithmetic operations: like add, sub, mult, div ').lower().strip()
 
-    if operation == 'add':
-        result = a+b
-    elif operation == 'sub':
-        result = a-b
-    elif operation == 'mult':
-        result = a*b
-    elif operation == 'div':
-        result = a/b if b != 0 else 'denominator cannot be zero'
-    else:
-        print('invalid input')
-    print('result: ', result)
+        if operation == 'add':
+            result = a+b
+        elif operation == 'sub':
+            result = a-b
+        elif operation == 'mult':
+            result = a*b
+        elif operation == 'div':
+            result = a/b if b != 0 else 'denominator cannot be zero'
+        else:
+            print('invalid input')
+        print('result: ', result)
+        continue_calculation = input("do you want to continue? (yes/y/no/n): ").lower().strip()
+        if continue_calculation not in ['yes', 'y']:
+            break
 
 basic_calculator()
