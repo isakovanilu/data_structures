6e94e66740e4281b4c5befbb21ac309d2725c6c8
Nilu Isakova <niluisakova@gmail.com>
2024-02-01 21:53:41 -0600

    atm check

--- ds_py/atm_test.py
+++ ds_py/atm_test.py
@@ -5,9 +5,13 @@ concept of maintaining state and handling user choices."""
 
 
 class DebitCard:
-    def __init__(self, card_id, pin, first_name, last_name, balance):
+    def __init__(self, card_id, pin, first_name, last_name, balance=100):
         self.card_id = card_id
         self.pin = pin
         self.first_name = first_name
         self.last_name = last_name
         self.balance = balance
+    
+    def check_balance(self):
+        print(f'Current balance: {self.balance}')
+        
