fdfd3d6f5b3d2ad18fb6173f5152e13977b3df08
Nilu Isakova <niluisakova@gmail.com>
2024-02-07 23:01:41 -0600

    todolist

---
+++ ds_py/todolist.py
@@ -0,0 +1,10 @@
+"""
+Todo List: Create a simple command-line 
+to-do list application where users can add tasks, 
+view all tasks, and delete a task. 
+This will introduce arrays or arraylists and the basics of 
+CRUD (Create, Read, Update, Delete) operations.
+"""
+class ToDoList():
+    def __init__(self):
+        pass
