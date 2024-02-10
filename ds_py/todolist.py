3e953c554bca6a52cde220ee555e4275e70863a1
Nilu Isakova <niluisakova@gmail.com>
2024-02-10 17:51:38 -0600

    todolist

--- ds_py/todolist.py
+++ ds_py/todolist.py
@@ -6,5 +6,10 @@ This will introduce arrays or arraylists and the basics of
 CRUD (Create, Read, Update, Delete) operations.
 """
 class ToDoList():
-    def __init__(self):
-        pass
+    def __init__(self, todo_lists=None):        
+        if todo_lists is None:
+            todo_lists = []
+        self.todo_lists = todo_lists
+        
+
+        
